0810808efa640e74d40f8fbcee1c608e76c6df1e
LavenderIris <pchung@gmail.com>
2018-01-05 15:27:16 -0800

    first java program

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My Name is First Name");
+        System.out.println("I am age years old");
+        System.out.println("My hometown is Hometown City");
+
+    }
+}
